e29c9357a25e8e741a368996a41935b0e04cef4e
matthew-l-craig <91706893+matthew-l-craig@users.noreply.github.com>
2021-09-30 20:48:29 -0400

    Add files via upload

---
+++ ames_script.R
@@ -0,0 +1,11 @@
+#Make library calls.
+library(dplyr)
+library(ggplot2)
+library(corrplot)
+library(RColorBrewer)
+library(car)
+library(mice)
+library(leaps)
+
+#Load the Ames housing dataset.
+Ames <- read.csv(file = 'AmesHousing.csv')
